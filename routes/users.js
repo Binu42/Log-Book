08ac8ee8a23b6661c8c92b841c3f9f8d789dd494
Binu42 <kbinu42@gmail.com>
2020-02-19 00:05:33 +0530

    user model with express-validator

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,22 @@
 const express = require("express");
-
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
+
+const User = require('../models/User');
 
 // @route       POST /api/users
 // @desc        Register users
 // @access      public
-router.post('/', (req, res) => {
-  res.send('sdakjf');
+router.post('/', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'please enter valid email').isEmail(),
+  check('password', 'please enter password with atleast 6 character').isLength({ min: 6 })
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  res.send(req.body);
 })
 
 
